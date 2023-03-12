14a954bf373bfcacae5d6cc681d82df8c3dbf305
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-03-12 13:08:38 +0000

    Add model of tweets and spinner function

--- advertList/advertView.js
+++ advertList/advertView.js
@@ -1,8 +1,21 @@
 export function buildAdvertView(advert) {
     const newAdvertElement = document.createElement('article');
-    newAdvertElement.innerHtml = `
-        <p>${advert.nombre}</p>
-`;
+    newAdvertElement.classList.add('advert')
+    newAdvertElement.innerHTML = `<p>${advert.pseudo}</p>
+    <img src="${advert.avatar}" />
+    <p>${advert.handle}</p>
+    <div class="user-info">
+    <span>${advert.handle}</span>
+    <img src="${advert.avatar}" />
+    </div>
+    <p>${advert.content} 
+    `;
 
     return newAdvertElement;
 }
+
+export function buildSpinnerView() {
+    const spinnerDiv = document.createElement('div');
+    spinnerDiv.classList.add('spinner');
+    return spinnerDiv;
+  };
