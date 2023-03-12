11a6bf8e1b0afe037a21a20fec979a941b64b5aa
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-03-12 17:31:03 +0000

    Change advert view

--- advertList/advertView.js
+++ advertList/advertView.js
@@ -1,15 +1,21 @@
 export function buildAdvertView(advert) {
     const newAdvertElement = document.createElement('article');
     newAdvertElement.classList.add('advert')
-    newAdvertElement.innerHTML = `<p>${advert.pseudo}</p>
-    <img src="${advert.avatar}" />
-    <p>${advert.handle}</p>
-    <div class="user-info">
-    <span>${advert.handle}</span>
-    <img src="${advert.avatar}" />
-    </div>
-    <p>${advert.content} 
-    `;
+    newAdvertElement.innerHTML = 
+    `<p>${advert.nombre}</p>
+    <p>${advert.descripcion}</p>
+    <p>${advert.tipo}</p>
+    <p>${advert.precio}</p>
+    <p>${advert.imagen}</p>
+    `; 
+    
+    // `<p>${advert.pseudo}</p>
+    // <img src="${advert.avatar}" />
+    // <div class="user-info">
+    // <span>${advert.handle}</span>
+    // </div>
+    // <p>${advert.content} 
+    // `;
 
     return newAdvertElement;
 }
