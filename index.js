95f5b5e7414dc9c44ca674d3fd9e1f47820df82d
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-03-12 13:09:07 +0000

    Add advert list controler

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { advertListController } from "./advertList/advertListController.js";
 
 const advertListElement = document.querySelector('.adverts-list');
-const loadAdvertsButtonElement = document.querySelector('#loadAdvertsButton');
 
-loadAdvertsButtonElement.addEventListener('click', () => {
-    advertListController(advertListElement)
- })
+advertListController(advertListElement);
+
+
+
+
 
 
