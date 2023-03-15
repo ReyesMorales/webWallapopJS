e25c3aecb1000dda1359536720acf3c287907305
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-03-15 19:36:42 +0000

    Add notificationController

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+import { notificationController } from "./notifications/notificationController.js";
 import { advertListController } from "./advertList/advertListController.js";
 
+const notificationsElement = document.querySelector('.notifications');
 const advertListElement = document.querySelector('.adverts-list');
 
+notificationController(notificationsElement);
 advertListController(advertListElement);
 
 
