f4a20ff0f6a6d71ee265ef7a888894bfd3a1083c
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-06-16 20:04:37 +0100

    Delete debbuger and console.logs

--- advertDetail/advertDetailController.js
+++ advertDetail/advertDetailController.js
@@ -8,13 +8,11 @@ export const advertDetailController = async (advertDetailElement, advertId) => {
     advertDetailElement.innerHTML = buildAdvertDetail(advert);
 
     const deleteBtn = document.querySelector(".delete-btn");
-    console.log("deleteButton: ", deleteBtn);
 
     if (deleteBtn) {
       deleteBtn.addEventListener("click", async () => {
         const token = localStorage.getItem("token");
         const userId = localStorage.getItem("userId");
-        console.log(token, userId);
         if (!token || !userId) {
           alert("Debe estar logeado para borrar un anuncio");
           return;
@@ -29,7 +27,6 @@ export const advertDetailController = async (advertDetailElement, advertId) => {
         );
 
         if (confirmDelete) {
-          debugger;
           try {
             await deleteAdvert(advert.id, token);
             alert("Anuncio borrado correctamente");
