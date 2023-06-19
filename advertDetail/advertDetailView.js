a9fee436e26d1298052ef08a7ecb5ce7d5d75472
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-06-19 18:07:16 +0100

    Modify capital letter

--- advertDetail/advertDetailView.js
+++ advertDetail/advertDetailView.js
@@ -1,7 +1,7 @@
 export const buildAdvertDetail = (advert) => {
     return `
     <p>Nombre: ${advert.nombre}</p>
-    <p>descripción: ${advert.descripcion}</p>
+    <p>Descripción: ${advert.descripcion}</p>
     <p>Tipo: ${advert.tipo}</p>
     <p>Precio: ${advert.precio}</p>
     <p>${advert.imagen}</p>
